9dfad29af313fe54f3123bff83e06b4e6af17af6
Bezo786 <behzadfarzan11@gmail.com>
2022-09-08 11:52:12 +0200

    hi

--- 1HelloWordl/Program.cs
+++ 1HelloWordl/Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+namespace Konsolenausgabe
+{
+    internal class Program
+    {
+        static void Main()
+        {
+
+            Console.WriteLine(".-.");
+            Console.WriteLine("(o o)");
+            Console.WriteLine("| O |");
+            Console.WriteLine("| |");
+            Console.WriteLine("'~~~'");
+          
+            Console.ReadKey();
+
+
+
+            //Hier Quellcode einfügen
+        }
+    }
+}
+
+
+namespace Aufgabe
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string Vorname = "Behzad";
+            string Nachname = "Farzan";
+            String Alter = "19";
+            Console.ReadKey();
+            //Hier Quellcode einfügen
+        }
+    }
+}
+
+
+namespace Aufgabe
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string Vorname = Console.ReadLine("Behzad");
+            string Nachname = Console.ReadLine("Farzan");
+            String Alter = Console.ReadLine("19");
+            Console.ReadKey();
+            //Hier Quellcode einfügen
+        }
+    }
+}
+
+
+
