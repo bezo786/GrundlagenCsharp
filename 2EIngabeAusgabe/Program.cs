7891313c586ad2f7c097daa5bf96480c25230702
Bezo786 <behzadfarzan11@gmail.com>
2022-09-08 12:56:14 +0200

    Heute

--- 2EIngabeAusgabe/Program.cs
+++ 2EIngabeAusgabe/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine(".-.");
+Console.WriteLine("(o o)");
+Console.WriteLine("| O |");
+Console.WriteLine("| |");
+Console.WriteLine("'~~~'");
+
+Console.WriteLine("Enter drücken...");
+Console.ReadKey();
+
+string Vorname = "Behzad";
+string Nachname = "Farzan";
+String Alter = "19";
+
+Console.WriteLine(Vorname);
+Console.WriteLine(Nachname);
+Console.WriteLine(Alter); 
+
+
+//Console.ReadKey();
+////Hier Quellcode einfügen
+
+
+
+//string vorname = Console.ReadLine();
+//string nachname = Console.ReadLine();
+//String alter = Console.ReadLine();
+//Console.ReadKey();
